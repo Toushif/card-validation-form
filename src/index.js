b5577df53a45961763c9d956705306231f014c8c
Toushif <toushif.haq@gmail.com>
2021-11-20 05:02:54 +0530

    card validation changes

--- src/index.js
+++ src/index.js
@@ -13,50 +13,76 @@ let tempValidThru = [];
 
 function submitForm(e) {
     e.preventDefault();
-    console.log('Form Submitted')
+    console.log("Form Submitted");
     return false;
 }
+
+formCardValidation.onsubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(formCardValidation);
+    const obj = Object.fromEntries(formData.entries());
+    console.log("form", formCardValidation, formData, obj);
+    // let response = await fetch("/article/formdata/post/user-avatar", {
+    //     method: "POST",
+    //     body: new FormData(formCardValidation),
+    // });
+
+    // let result = await response.json();
+
+    // alert(result.message);
+};
 
 function handleValidation(e) {
     const name = e.target.name;
     const value = e.target.value;
-    name === 'cardNumber' ? isCardNumber = name === 'cardNumber' && value.length === 16 : '';
-    name === 'cardName' ? isCardName = name === 'cardName' && value.length >= 8 : '';
-    name === 'validThru' ? isValidThru = name === 'validThru' && value.length === 4 : '';
-    name === 'cardCvv' ? isCardCvv = name === 'cardCvv' && value.length === 3 : '';
+    name === "cardNumber"
+        ? (isCardNumber = name === "cardNumber" && value.length === 16)
+        : "";
+    name === "cardName"
+        ? (isCardName = name === "cardName" && value.length >= 8)
+        : "";
+    name === "validThru"
+        ? (isValidThru = name === "validThru" && value.length === 4)
+        : "";
+    name === "cardCvv"
+        ? (isCardCvv = name === "cardCvv" && value.length === 3)
+        : "";
 
-    if(isCardNumber && isCardName && isValidThru && isCardCvv) {
-        submitBtn.removeAttribute('disabled')
+    if (isCardNumber && isCardName && isValidThru && isCardCvv) {
+        submitBtn.removeAttribute("disabled");
     } else {
-        if(!submitBtn.getAttribute('disabled')) submitBtn.setAttribute('disabled', true)
+        if (!submitBtn.getAttribute("disabled"))
+            submitBtn.setAttribute("disabled", true);
     }
-    console.log(e.target.name, isCardNumber,isCardName, isValidThru, isCardCvv)
+    // console.log(
+    //     e.target.name,
+    //     isCardNumber,
+    //     isCardName,
+    //     isValidThru,
+    //     isCardCvv
+    // );
 }
 
 function handleCardNumberInput(e) {
     const value = e.target.value;
-    if(value.length && value.length > 4) {
-        tempValidThru = []
-        for(let i = 0; i < value.length; i+=4) {
-            tempValidThru.push(value.substr(i, 4))
+    if (value.length && value.length > 4) {
+        tempValidThru = [];
+        for (let i = 0; i < value.length; i += 4) {
+            tempValidThru.push(value.substr(i, 4));
         }
     }
-    const store = tempValidThru.join('-')
-    // setTimeout(_=> cardNum.setAttribute('value', store), 0)
-    cardNum.setAttribute('defaultValue', store); //formatting not working
-    console.log('temp', e, tempValidThru, store, cardNum.value)
+    const store = tempValidThru.join("-");
+    cardNum.setAttribute("defaultValue", store); //formatting not working
+    // console.log("temp", e, tempValidThru, store, cardNum.value);
 }
 
 function init() {
-    submitBtn.setAttribute('disabled', true)
-    cardNum.addEventListener('change', handleValidation, false)
-    // var event = new Event('keyup');
-    cardNum.addEventListener('keyup', handleCardNumberInput, false)
-    // cardNum.dispatchEvent(event);
-    cardName.addEventListener('change', handleValidation, false)
-    validThru.addEventListener('change', handleValidation, false)
-    cardCvv.addEventListener('change', handleValidation, false)
+    submitBtn.setAttribute("disabled", true);
+    cardNum.addEventListener("keydown", handleValidation, false);
+    cardNum.addEventListener("keyup", handleCardNumberInput, false);
+    cardName.addEventListener("keydown", handleValidation, false);
+    validThru.addEventListener("keydown", handleValidation, false);
+    cardCvv.addEventListener("keydown", handleValidation, false);
 }
 
-init()
-
+init();
